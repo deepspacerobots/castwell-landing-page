849055aee56f43f639ad950483acd3a5cd3eea48
Frank Lujan <daniel@deepspacerobots.com>
2026-01-26 16:57:51 -0600

    fix typo and update all instances of Castwell to CastWell

--- src/confirm-email.js
+++ src/confirm-email.js
@@ -1,30 +1,30 @@
-const title = document.getElementById('title');
-const message = document.getElementById('message');
+const title = document.getElementById("title");
+const message = document.getElementById("message");
 
 const hash = new URLSearchParams(window.location.hash.substring(1));
 
-const error = hash.get('error');
-const errorDescription = hash.get('error_description');
-const type = hash.get('type');
-const access_token = hash.get('access_token');
-const expires_at = hash.get('expires_at');
+const error = hash.get("error");
+const errorDescription = hash.get("error_description");
+const type = hash.get("type");
+const access_token = hash.get("access_token");
+const expires_at = hash.get("expires_at");
 
 const setError = () => {
-	title.textContent = 'Something went wrong';
-	message.textContent =
-		errorDescription?.replaceAll('+', ' ') ||
-		'The link is invalid or has expired.';
+  title.textContent = "Something went wrong";
+  message.textContent =
+    errorDescription?.replaceAll("+", " ") ||
+    "The link is invalid or has expired.";
 };
 
-if (error || type !== 'email_change') {
-	setError();
+if (error || type !== "email_change") {
+  setError();
 } else {
-	// const now = new Date();
-	if (!access_token) {
-		setError();
-	} else {
-		title.textContent = 'Email verified successfully';
-		message.textContent =
-			'Your email address has been successfully verified. You can now close this window and continue using Castweel without any issues.';
-	}
+  // const now = new Date();
+  if (!access_token) {
+    setError();
+  } else {
+    title.textContent = "Email verified successfully";
+    message.textContent =
+      "Your email address has been successfully verified. You can now close this window and continue using CastWell without any issues.";
+  }
 }
